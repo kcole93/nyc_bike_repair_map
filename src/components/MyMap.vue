44636ca59b439f0d700360bebd92f76429166894
Kevin <kcole93@gmail.com>
2022-08-29 18:28:47 -0400

    Move Zoom Control to RH side

--- src/components/MyMap.vue
+++ src/components/MyMap.vue
@@ -1,5 +1,6 @@
 <template>
-    <l-map id="map" ref="map" v-model:zoom="zoom" :center="[40.619888, -73.917170]" zoomDelta=0.5 :maxBounds="[[40.522151, -74.381561],[40.951900, -73.26438]]" minZoom=9>
+    <l-map id="map" ref="map" v-model:zoom="zoom" :center="[40.619888, -73.917170]" zoomDelta=0.5 :maxBounds="[[40.522151, -74.381561],[40.951900, -73.26438]]" :minZoom=9 :options="{zoomControl: false}">
+      <l-control-zoom> </l-control-zoom>
       <l-tile-layer
         url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
         layer-type="base"
@@ -21,7 +22,7 @@
   <script setup>
   import { ref } from 'vue';
   import ConditionTag from '../components/ConditionTag.vue';
-  import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
+  import { LMap, LTileLayer, LMarker, LPopup, LControlZoom } from "@vue-leaflet/vue-leaflet";
   import "leaflet/dist/leaflet.css";
   
   const zoom = ref(12)
@@ -39,12 +40,9 @@
   </script>
 
   <style scoped>
-  
   #map {
     z-index: 1;
   }
 
-  .leaflet-bottom {
-    z-index: 1;
-  }
+  .leaflet-control-container { position: absolute; right: 56px }
   </style>
